7d27e884a4c4d2d5609a6c9ba16b8f351f33fecd
austin-mel <austin.csus@gmail.com>
2025-07-08 12:52:59 -0700

    Adding libraries and dependencies

---
+++ personal-website/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  </header>
+
+  <main>
+
+  </main>
+</template>
